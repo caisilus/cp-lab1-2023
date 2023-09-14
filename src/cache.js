d0e8ea2e69fe962f84d996be7fbf0bf41308da2b
caisilus <serangor@gmail.com>
2023-09-14 12:33:16 +0300

    Add basic key value storage

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,15 @@
 class Cache{
+    constructor () {
+        this.data = new Map()
+    }
+    save(key, value) {
+        this.data[key] = value
+    }
 
+    get(key) {
+        const value = this.data[key]
+        
+        return value === undefined ? null : value
+    }
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,23 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
-});
+describe('cache class', () => {
+    it('should save data at least for one query', () => {
+        const key = "key";
+        const value = "value";
+        let cache = new Cache();
+
+        cache.save(key, value);
+
+        let cached_value = cache.get(key);
+        expect(cached_value).toBe(value);
+    });
+    
+    it('should return null when there is no value by key', () => {
+        const key = "key";
+        let cache = new Cache();
+
+        let cached_value = cache.get(key);
+
+        expect(cached_value).toBe(null);
+    });
+})
