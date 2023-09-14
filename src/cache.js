723e1f09771bb8276b9baefc6cecd4d3c6edae93
caisilus <serangor@gmail.com>
2023-09-14 12:59:25 +0300

    Change api and add number of queries feature

--- src/cache.js
+++ src/cache.js
@@ -2,14 +2,27 @@ class Cache{
     constructor () {
         this.data = new Map()
     }
-    save(key, value) {
-        this.data[key] = value
+    set(key, value, number_of_queries) {
+        this.data.set(key, {
+            value: value,
+            number_of_queries: number_of_queries
+        });
     }
 
     get(key) {
-        const value = this.data[key]
-        
-        return value === undefined ? null : value
+        const valueObject = this.data.get(key)
+        if (valueObject === undefined) {
+            return null
+        }
+
+        if (valueObject.number_of_queries === 0) {
+            this.data.delete(key)
+            return null;
+        }
+
+        valueObject.number_of_queries -= 1;
+
+        return valueObject.value
     }
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,12 +1,12 @@
 import {Cache} from "../src/cache";
 
-describe('cache class', () => {
-    it('should save data at least for one query', () => {
+describe('chache get/set functionality', () => {
+    it('should set data at least for one query', () => {
         const key = "key";
         const value = "value";
         let cache = new Cache();
 
-        cache.save(key, value);
+        cache.set(key, value);
 
         let cached_value = cache.get(key);
         expect(cached_value).toBe(value);
@@ -18,6 +18,21 @@ describe('cache class', () => {
 
         let cached_value = cache.get(key);
 
+        expect(cached_value).toBe(null);
+    });
+
+    it('should return null after setted number of queries', () => {
+        const key = "key";
+        const value = "value"
+        const number_of_queries = 2;
+        let cache = new Cache();
+
+        cache.set(key, value, number_of_queries);
+        
+        cache.get(key);
+        cache.get(key);
+        
+        const cached_value = cache.get(key);
         expect(cached_value).toBe(null);
     });
 })
