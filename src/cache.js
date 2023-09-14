308d9243cacb48d2c1ef059904d3812d690085ab
caisilus <serangor@gmail.com>
2023-09-14 15:07:48 +0300

    Refactoring

--- src/cache.js
+++ src/cache.js
@@ -3,20 +3,20 @@ class Cache{
         this.data = new Map()
         this.statisticsList = []
     }
+
     set(key, value, numberOfQueries=1) {
         this.data.set(key, {
             value: value,
             numberOfQueries: numberOfQueries
         });
-        this.statisticsList.push(`set ${key}, ${value}, ${numberOfQueries}`)
+        this.statisticsList.push(this.set_statistics_line(key, value, numberOfQueries))
     }
 
     get(key) {
         const valueObject = this.data.get(key)
-        if (valueObject === undefined) {
-            return null
-        }
 
+        if (valueObject === undefined) { return null }
+        
         if (valueObject.numberOfQueries === 0) {
             this.data.delete(key)
             return null;
@@ -24,8 +24,16 @@ class Cache{
 
         valueObject.numberOfQueries -= 1;
 
-        this.statisticsList.push(`get ${key} ${valueObject.numberOfQueries}`)
+        this.statisticsList.push(this.get_statistics_line(key, valueObject.numberOfQueries))
         return valueObject.value
+    }
+
+    get_statistics_line(key, numberOfQueries) {
+        return `get ${key} ${numberOfQueries}` 
+    }
+
+    set_statistics_line(key, value, numberOfQueries) {
+        return `set ${key}, ${value}, ${numberOfQueries}`
     }
 
     statistics() {
